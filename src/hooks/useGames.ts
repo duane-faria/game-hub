2bf4dad5cd9f747404a2ec5895fc5fe72e7e4b09
Duane Faria <duane_gabriel@hotmail.com>
2023-10-28 19:29:30 -0300

    feat: create a use games hook to hold the logic of getting games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import React from "react";
+
+import apiClient from "../services/api-client";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+interface UseGames {
+    error: string;
+    games: Game[];
+}
+
+export default function useGames(): UseGames {
+    const [games, setGames] = React.useState<Game[]>([]);
+    const [error, setError] = React.useState('');
+
+    React.useEffect(() => {
+        apiClient.get<FetchGamesResponse>('/games')
+            .then((response) => {
+                setGames(response.data.results)
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    }, []);
+
+    return {
+        error,
+        games,
+    }
+
+}

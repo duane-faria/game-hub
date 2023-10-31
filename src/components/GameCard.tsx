48920be0b45d0665344d03b41c606092a5334ddd
duane.faria <duane_gabriel@hotmail.com>
2023-10-31 01:52:13 -0300

    feat: add platform list to game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,22 +1,25 @@
 import { FunctionComponent } from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import PlatformList from "./PlatformList";
+import { getCroppedImageUrl } from "../services/image-crop";
 
 interface GameCardProps {
     game: Game;
 }
- 
-const GameCard: FunctionComponent<GameCardProps> = ({game}: GameCardProps) => {
-    return ( 
-        <Card overflow="hidden">
-            <Image src={game.background_image} height="100px" objectFit="fill"/>
+
+const GameCard: FunctionComponent<GameCardProps> = ({ game }: GameCardProps) => {
+    return (
+        <Card overflow="hidden" width="300px">
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize='2xl'>
                     {game.name}
                 </Heading>
+                <PlatformList platforms={game.parent_platforms.map(({ platform }) => platform)} />
             </CardBody>
         </Card>
-     );
+    );
 }
- 
+
 export default GameCard;

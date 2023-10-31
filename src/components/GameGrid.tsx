e5f0bd8d7fc2b5d15e77ba63b62bd193b715c78f
duane.faria <duane_gabriel@hotmail.com>
2023-10-31 01:47:53 -0300

    add: game card skeleton to game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,20 +2,30 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
-    const { error, games } = useGames();
+  const { games, loading, error } = useGames();
+  const skeletonItems = [1, 2, 3, 4, 5, 6];
 
-    return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} padding="10px" spacing={10} borderRadius={10}>
-                {games.map((game) => (
-                    <GameCard key={game.id} game={game} />
-                ))}
-            </SimpleGrid>
-        </>
-    );
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3 }}
+        padding="10px"
+        spacing={10}
+        borderRadius={10}
+      >
+        {loading &&
+          skeletonItems.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
 }
 
 export default GameGrid;
